cf7868c6b05352741441450c7f5c2163776b5dcb
Brad Heath <brad.heath@gmail.com>
2017-03-21 13:43:56 -0400

    update sh

---
+++ bigcases.sh
@@ -0,0 +1,3 @@
+python pacer-rss.py
+python bigcases_scrape_docs.py
+python bigcases.py
--- pacer-rss.sh
+++
@@ -1,3 +0,0 @@
-python /data/scripts/pacer-rss.py
-python /data/scripts/bigcases_scrape_docs.py
-python /data/scripts/bigcases.py
